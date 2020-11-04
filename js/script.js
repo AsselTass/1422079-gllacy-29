4d43556fdad9bffb95ca1f14f20b9a8c056b4522
AsselTass <45459677+AsselTass@users.noreply.github.com>
2020-11-04 17:42:11 +0100

    Декоративные элементы на внутренней странице

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var cart_popup = document.querySelector('.cart-list');
+cart_popup.classList.add('cart-list--show');
+
+var openPopupButton = document.querySelector('.cart-link');
+openPopupButton.addEventListener('click', function () {
+
+});
