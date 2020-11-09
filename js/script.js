0ee97646e8bd92dccd055cbcde2e89027aee6b87
AsselTass <45459677+AsselTass@users.noreply.github.com>
2020-11-09 16:16:30 +0100

    завершение

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 const feedbackLink = document.querySelector(".contacts-button");
 const contactPopup = document.querySelector(".modal");
 const contactClose = contactPopup.querySelector(".modal-close");
+const popupOverlay = document.querySelector(".overlay");
 
 
 feedbackLink.addEventListener("click", function (evt) {
   evt.preventDefault();
   contactPopup.classList.add("modal-show");
+  popupOverlay.classList.add("modal-show");
 });
 
 contactClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   contactPopup.classList.remove("modal-show");
+  popupOverlay.classList.remove("modal-show");
 });
